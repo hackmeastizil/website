6c8845963daff42f1840b3c633d82e151baa4a65
hackmeastizil <42996154+hackmeastizil@users.noreply.github.com>
2020-09-25 22:37:15 +0530

    Very First Commit
    This Commit was done on day 1

---
+++ header.php
@@ -0,0 +1,14 @@
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script defer src="theme.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://kit.fontawesome.com/6244fda63e.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet"/>
+
+
+
+  </head>
+</html>
